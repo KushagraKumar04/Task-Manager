7422805bccc590b41f1362ea8bc8b9437a16b103
Kushagra Kumar <167355875+KushagraKumar04@users.noreply.github.com>
2024-10-04 13:30:05 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ const Home = async ({}) => {
   return (
     <div className="h-screen">
       <h1 className="text-2xl sm:text-4xl font-black tracking-wide text-center pt-6 pb-10 sm:pb-24">
-        Welcome to Task Manager
       </h1>
 
       <div className="grid place-items-center">
